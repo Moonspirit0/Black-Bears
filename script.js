a8d97bc685aaadee5ca128bc0c17b4ba540be72f
Moonspirit0 <87946012+Moonspirit0@users.noreply.github.com>
2021-10-24 16:11:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function imagehover1(){
+    document.getElementById("imgHover1").src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/black-bear-crossing-log-in-woods-dan-friend.jpg";
+}
+
+function hide1(){
+    document.getElementById("p1").innerHTML="";
+}
+
+function hide2(){
+    document.getElementById("p2").innerHTML="";
+}
+
+function hide3(){
+    document.getElementById("p3").innerHTML="";
+}
+
+function hide4(){
+    document.getElementById("p4").innerHTML="";
+}
+
+function hide5(){
+    document.getElementById("p5").innerHTML="";
+}
+
+
+
+
+function show1(){
+    document.getElementById("p1").innerHTML="Not all black bears are black—their fur can range in color from pure white to a cinnamon color to very dark brown or black. Most populations have a mixture of these colors, including the pure white form, which is found in some individuals in the island archipelago in southern British Columbia (Kermodi Island). This white black bear (called a spirit bear, revered by Native Americans) is caused by a recessive gene from both the mother and the father. Genes can also result in the light gray coat color of the blue bear, or glacier bear, in southeastern Alaska. Despite these genetic variants, most of the bears in any region are black in color. Some bears have a white patch on their chests. They have a short, inconspicuous tail, longish ears, a relatively straight profile from nose to forehead, and small, dark eyes. There are several ways to tell a black bear from a grizzly bear. Black bears and grizzly bears can both have a wide variety of colors and sizes, but most commonly in areas where both species occur, black bears are smaller and darker than grizzly bears. Black bears have longer and less rounded ears and a more straight profile from forehead to nose compared to grizzly bears. Grizzly bears have larger shoulder humps and a more dished-in facial profile and much longer front claws that are evident in the tracks. Black bears in some areas where food is scarce are much smaller than in other areas where food is abundant. Typically adults are three feet tall at the shoulder, and their length from nose to tail is about 75 inches. All bears, including black bears, are sexually dimorphic—meaning adult males are much larger than adult females. A large male black bear can exceed 600 pounds in weight, while females seldom exceed 200 pounds";
+}
+
+function show2(){
+    document.getElementById("p2").innerHTML="American black bears are omnivorous, meaning they will eat a variety of things, including both plants and meat. Their diet includes roots, berries, meat, fish, insects, larvae, grass, and other succulent plants. They are able to kill adult deer and other hoofed wildlife but most commonly are only able to kill deer, elk, moose, and other hoofed animals when the prey are very young. They are able to kill livestock, especially sheep. Bears are very attracted to human garbage, livestock food or pet food, or other human-associated foods like fruit trees. Bears using these human-associated foods can quickly become habituated to them and this commonly results in the bears being killed as nuisances. This is true for bee hives as well, as bears are very attracted to honey.";
+}
+
+function show3(){
+    document.getElementById("p3").innerHTML="The American black bear's range covers most of the North American continent. They are found in Alaska, much of Canada and the contiguous United States, and extend as far south as northern Mexico. Because of their versatile diet, black bears can live in a variety of habitat types. They inhabit both coniferous and deciduous forests, as well as open alpine habitats. They typically don't occur on the Great Plains or other wide-open areas, except along river courses where there is riparian vegetation and trees. They can live just about anywhere they can find food, but largely occur where there are trees.";
+}
+
+function show4(){
+    document.getElementById("p4").innerHTML="Hello";
+}
+
+function show5(){
+    document.getElementById("p5").innerHTML="Hello";
+}
